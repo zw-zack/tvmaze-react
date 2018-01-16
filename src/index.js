b793995624f4f33a1d5ce49a97d966f6515669df
superbuggy <jpqreichard@gmail.com>
2018-01-16 11:16:47 -0500

    add build

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './Home';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-
-ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
